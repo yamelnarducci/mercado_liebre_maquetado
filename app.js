9d4ec533c180bca6255aad8927d8bcc26fa1c222
yamelnarducci <yayu.narducci@gmail.com>
2023-11-12 20:55:44 -0300

    Feat: formularios-revision de adaptativo y flexbox

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PORT = 3030;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
-
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views', 'register.html')));
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views', 'login.html')));
+app.get('/profile',(req,res) => res.sendFile(path.join(__dirname,'views', 'profile.html')));
 
 app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`));
